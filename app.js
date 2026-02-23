059446226bfe54af98de05e342ed4424c3b0d8d0
KasunInd27 <kasunindrajith27@gmail.com>
2026-02-23 12:58:23 +0530

    Implement file write functionality using fs.writeFile

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+//Write to file 
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
+
+
