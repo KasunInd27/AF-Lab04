26745f87c2b22e417ce655896d4ad7326fe962aa
KasunInd27 <kasunindrajith27@gmail.com>
2026-02-23 12:44:55 +0530

    Add file read example

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("NodeJS Lab 04 - Initial Setup");
+//console.log("NodeJS Lab 04 - Initial Setup");
+
+//Read file
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
