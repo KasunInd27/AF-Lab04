0ebab99a72d748fe48dc37190fa9cab94d03b0d2
KasunInd27 <kasunindrajith27@gmail.com>
2026-02-23 13:17:38 +0530

    Implement custom module and demonstrate module import

--- app.js
+++ app.js
@@ -41,4 +41,7 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
   console.log("Error: " + err.message);
 });
 
+//Importing a custom module
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
 
